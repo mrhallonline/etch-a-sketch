ca27c9132ef5fb21e28b4043f3dea73dbe4dee26
Kevin <mrhallonline@gmail.com>
2019-10-25 19:15:12 -0500

    set up forms to collect canvas size text entry using jQuery

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-
-for (var i = 1; i<257; i++){
+for (var i = 1; i < 257; i++) {
     $("#gridBox").prepend('<div class="grid-item"></div>');
 }
 
-$(".grid-item").hover(function(){
-$(this).addClass("onHover");
+$(".grid-item").hover(function () {
+    $(this).addClass("onHover");
 });
 
-$("#clearCanvas").click(alert("button click"));
+$("#clearCanvas").click(function () {
+    location.reload(false);
+});
+
+
+$(".canvasButton").click(function () {
+
+    canvasSize = $("#vertical").val();
+
+    alert(canvasSize);
+});
