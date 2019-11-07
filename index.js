e03bd35e24727809a5ddb86d2134d2a5d3241064
Kevin <mrhallonline@gmail.com>
2019-11-07 01:03:28 -0500

    add random color picker

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+//sets up the initial 16x16 grid
 for (var i = 1; i < 257; i++) {
     $("#gridBox").prepend('<div class="grid-item"></div>');
 }
 
+//hovering over a grid changes the color from white to black 
 $(".grid-item").hover(function () {
     $(this).addClass("onHover");
 });
 
+//clears the grid back to original state
 $("#clearCanvas").click(function () {
     location.reload(false);
 });
 
 
+//sets up new canvas with box sizes based on the number entered.
 $(".canvasButton").click(function () {
-    // location.reload(true);
+    //stores the number entered
     var canvasSize = $("#vertical").val();  
     
+    //new grid based on canvasSize variable
     $(".grid-container").empty().css({
     'display': 'grid',
     'margin': '5px auto',
@@ -25,12 +30,26 @@ $(".canvasButton").click(function () {
     'padding': '50px'
      });
     
+     //populates enough grid containers based on canvasSize variable
     for (var i = 1; i < (canvasSize*canvasSize+1); i++) {
         $(".grid-container").prepend('<div class="grid-item"></div>');
     }
-    
-    $(".grid-item").hover(function () {
-        $(this).addClass("onHover");
-    });
-
+    //hovering changes white boxes to black
+    // $(".grid-item").hover(function () {
+    //     $(this).addClass("onHover");
+    // });
+    randomColor()
 });
+
+function randomColor(){
+    $(".grid-item").hover(function(){
+        
+        let x = Math.floor(Math.random()*256);
+        let y = Math.floor(Math.random()*256);
+        let z = Math.floor(Math.random()*256);
+        let newColor = 'rgb('+x+','+y+','+z+')';
+        $(this).css('background-color' , newColor);
+    });
+    
+    // alert(newColor);
+}
