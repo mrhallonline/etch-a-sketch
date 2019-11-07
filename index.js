f6ee4503bb0928370ea4ff84570aa9781b4c2b1b
Kevin <mrhallonline@gmail.com>
2019-11-07 00:06:46 -0500

    add empty method to remove children of gridbox. It works now with no need to reload page

--- index.js
+++ index.js
@@ -11,27 +11,26 @@ $("#clearCanvas").click(function () {
 });
 
 
-
-
-
 $(".canvasButton").click(function () {
-    // location.reload(false);
+    // location.reload(true);
     var canvasSize = $("#vertical").val();  
     
-    $(".grid-container").css({
+    $(".grid-container").empty().css({
     'display': 'grid',
     'margin': '5px auto',
-    'height': '600px',
-    'width': '600px',
-    // 'background-color': '#2196F3',
+    'height': '800px',
+    'width': '800px',
     'grid-template-columns':  'repeat(' + canvasSize + ', 1fr)',
-    
+    'grid-template-rows':  'repeat(' + canvasSize + ', 1fr)',
     'padding': '50px'
      });
     
     for (var i = 1; i < (canvasSize*canvasSize+1); i++) {
-        $(".grid-container").append('<div class="grid-item"></div>');
+        $(".grid-container").prepend('<div class="grid-item"></div>');
     }
     
+    $(".grid-item").hover(function () {
+        $(this).addClass("onHover");
+    });
 
 });
